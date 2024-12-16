dcb6ca03e6e6f7c61d28762c18d970ad253ff90d
uiux@okdollar frontend <uiux@okdollar.com>
2024-12-16 15:59:27 +0630

    card

--- src/app/pages/profile-card/profile-card.component.ts
+++ src/app/pages/profile-card/profile-card.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
+
 
 @Component({
   selector: 'app-profile-card',
@@ -8,8 +9,13 @@ import {MatCardModule} from '@angular/material/card';
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.css'
 })
-export class ProfileCardComponent {
+export class ProfileCardComponent implements OnInit {
   longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
   from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
   originally bred for hunting.`;
+  constructor(){}
+  Image:any;
+  ngOnInit(): void {
+    this.Image = '/assets/images/boy.jpg'
+  }
 }
